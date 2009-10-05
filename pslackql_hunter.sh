ef844cab5eea1e71650080f9bde8ef9784d9e62f
Esther Schneeweisz <astera@hackerspaces.org>
2009-10-05 21:01:38 +0200

    added functionality: looks for existing file (migration!), only affects SELECT queries. cheers.

--- pslackql_hunter.sh
+++ pslackql_hunter.sh
@@ -2,4 +2,10 @@
 
 # ohai thar, plz lemme terminat0r ur delicious soup queriez that ran longer than 10 min, kthxbai
 
-psql -c "SELECT NOW(),procpid,NOW()-query_start,current_query,pg_cancel_backend(procpid) FROM pg_stat_activity WHERE NOW()-query_start>TIME '00:10:00';" -t postgres >> pslackql_huntreport.txt
+FILE=""
+
+if [ -e $FILE ]; then
+    echo `date` 'Running migration' >> pslackql_huntreport.txt
+else 
+    psql -c "SELECT NOW(),procpid,NOW()-query_start,current_query,pg_cancel_backend(procpid) FROM pg_stat_activity WHERE current_query LIKE "SELECT%" AND NOW()-query_start>TIME '00:10:00';" -t postgres >> pslackql_huntreport.txt
+fi
