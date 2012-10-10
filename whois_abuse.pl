76c080ab8806789bcb59c8c409e6161141aa6cd0
Esther Schneeweisz <astera@hackerspaces.org>
2012-10-10 21:56:39 +0200

    Get the abuse email addresses for multiple IPs fast.

---
+++ whois_abuse.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl 
+use strict;
+
+my $scriptname = $0;
+@ARGV || die("Usage: $scriptname <IP>\n");
+
+while (@ARGV) {
+	&whois(shift);
+}
+
+sub whois
+{	my($arg) = @_;
+	my($line);
+	my(%email) = ();
+	
+	if (open(WHOIS, "whois $arg |")) {
+		while (defined($line = <WHOIS>)) {
+			chop $line;
+			$line =~ /([\w-.]+\@[\w-.]+)/ && ($email{$1} = 1);
+		}
+		close WHOIS;
+		print "Email complaints about ", $arg, " to: ", join(" | ", sort keys %email), "\n";
+	} else {
+		warn "Error: $!\n";
+	}
+}
